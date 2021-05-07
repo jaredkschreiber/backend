79b71659ee1f943e095abe641375a4a2a8491005
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-06 23:58:52 -0500

    V files for book list. Dropped the tables created in book-service

---
+++ apis/src/main/resources/db/migration/V4__booklists_ddl.sql
@@ -0,0 +1,33 @@
+--enum for book type
+create type list_type AS ENUM ('READ', 'READING', 'DNF', 'TOREAD');
+
+create table users (
+    id varchar(10) primary key,
+    created date
+);
+
+-- book list table
+create table booklists (
+    id varchar(25) primary key,
+    list_type list_type,
+    num_books int,
+    created date
+);
+
+-- table joining users and book lists
+create table users_booklists (
+    user_id varchar(10),
+    booklists_id varchar(25),
+    primary key (user_id, booklists_id),
+    foreign key (user_id) references users(id) on delete cascade,
+    foreign key (user_id) references booklists(id) on delete cascade
+);
+
+-- table joining books and book lists
+create table books_booklists (
+    book_id varchar(10),
+    booklists_id varchar(25),
+    primary key (book_id, booklists_id),
+    foreign key (book_id) references books(id) on delete cascade,
+    foreign key (booklists_id) references booklists(id) on delete cascade
+);
---
+++ apis/src/main/resources/db/migration/V6__new_book_lists_ddl.sql
@@ -0,0 +1,14 @@
+-- book list table
+create table book_lists (
+    id varchar(25) primary key,
+    list_type list_type,
+    num_books int
+);
+
+create table books_in_list(
+    list_id varchar(25),
+    book_in_list varchar(10),
+    primary key (list_id, book_in_list),
+    foreign key (list_id) references book_lists(id) on delete cascade
+);
+
