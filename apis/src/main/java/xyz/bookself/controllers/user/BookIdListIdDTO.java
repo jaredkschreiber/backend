2661f66338d89ec349d3db4b67c1640c95f51188
Jared Schreiber <mv3br8gsk3@protonmail.com>
2021-05-11 23:44:06 -0500

    moving book dto into its own file

---
+++ apis/src/main/java/xyz/bookself/controllers/user/BookIdListIdDTO.java
@@ -0,0 +1,22 @@
+package xyz.bookself.controllers.user;
+
+public class BookIdListIdDTO {
+    private String bookId;
+    private String listId;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getListId() {
+        return listId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
+}
