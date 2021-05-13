db7f99235f3659d5dd9d53a0b70b450005e87937
jaredkschreiber <67459608+jaredkschreiber@users.noreply.github.com>
2021-05-12 19:33:22 -0500

    moving book dto into its own file (#31)

--- apis/src/main/java/xyz/bookself/controllers/user/BookListController.java
+++ apis/src/main/java/xyz/bookself/controllers/user/BookListController.java
@@ -52,24 +52,3 @@ public class BookListController {
         return new ResponseEntity<>(foundBookList, HttpStatus.OK);
     }
 }
-
-class BookIdListIdDTO {
-    private String bookId;
-    private String listId;
-
-    public String getBookId() {
-        return bookId;
-    }
-
-    public String getListId() {
-        return listId;
-    }
-
-    public void setBookId(String bookId) {
-        this.bookId = bookId;
-    }
-
-    public void setListId(String listId) {
-        this.listId = listId;
-    }
-}
