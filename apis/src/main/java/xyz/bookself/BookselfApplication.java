9e1d4c85e361f1a0194340612b5e1df8b8bc513e
Kyle Olson <kyle@k-olson.org>
2021-05-31 16:56:50 -0400

    tweaks to config (#61)

--- apis/src/main/java/xyz/bookself/BookselfApplication.java
+++ apis/src/main/java/xyz/bookself/BookselfApplication.java
@@ -12,8 +12,7 @@ import xyz.bookself.config.BookselfCorsConfiguration;
 import java.util.TimeZone;
 
 /**
- * Runs the application with all AWS auto configuration resources disabled. They will be conditionally
- * used if the application detects it is running in AWS automatically.
+ * Runs the application with all AWS auto configuration resources disabled except for CloudWatch.
  */
 @SpringBootApplication(exclude = {
 		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
@@ -24,8 +23,7 @@ import java.util.TimeZone;
 		org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.metrics.CloudWatchExportAutoConfiguration.class
+		org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class
 })
 @ConfigurationPropertiesScan({ "xyz.bookself.config" })
 @Slf4j
