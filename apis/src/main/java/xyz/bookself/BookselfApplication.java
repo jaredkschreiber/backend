2bea45d98cdb8444714f1867b65d85bab4aa88b0
Kyle Olson <kyle@k-olson.org>
2021-05-31 17:17:41 -0400

    tweaks to config again (#62)

--- apis/src/main/java/xyz/bookself/BookselfApplication.java
+++ apis/src/main/java/xyz/bookself/BookselfApplication.java
@@ -12,18 +12,19 @@ import xyz.bookself.config.BookselfCorsConfiguration;
 import java.util.TimeZone;
 
 /**
- * Runs the application with all AWS auto configuration resources disabled except for CloudWatch.
+ * Runs the application with all AWS auto configuration resources disabled.
+ * They will be enabled via {@link xyz.bookself.config.AwsConfiguration} when this app is ran on AWS.
  */
 @SpringBootApplication(exclude = {
 		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.context.ContextCredentialsAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.context.ContextResourceLoaderAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class
+		org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.metrics.CloudWatchExportAutoConfiguration.class
 })
 @ConfigurationPropertiesScan({ "xyz.bookself.config" })
 @Slf4j
